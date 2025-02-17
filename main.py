8448ce8b0333f9c1f9ab10a2e5d673c8eb329be2
张岁末 <35794263+ZhangSuimo@users.noreply.github.com>
2025-02-17 16:21:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
     love_day = int(config["love_date"].split("-")[2])
     love_date = date(love_year, love_month, love_day)
     # 获取在一起的日期差
-    love_days = str(today.__sub__(love_date)).split(" ")[0]
+    love_days = int(str(today.__sub__(love_date)).split(" ")[0]) + 10000
     # 获取所有生日数据
     birthdays = {}
     for k, v in config.items():
